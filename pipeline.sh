c029f992f7d4dd8e234e9c8b9439ac47255e8e3f
ell1sg <15314622+ell1sg@users.noreply.github.com>
2024-02-22 23:59:25 -0800

    docs: update example pipeline and readme

--- pipeline.sh
+++ pipeline.sh
@@ -6,14 +6,7 @@ STARTTIME=`date`
 echo "Started ExpressionLncr pipeline @ $STARTTIME..."
 ./get_lncrna.py
 ./get_ensembl_probes.py
-mv data/lncrnas.bed data/lncrnas.unsorted.bed
-mv data/probes.bed data/probes.unsorted.bed
-# sort the bed files to speed up the next step.
-sort -k1,1 -k2,2n data/lncrnas.unsorted.bed > data/lncrnas.bed
-sort -k1,1 -k2,2n data/probes.unsorted.bed > data/probes.bed
-# -d flag is short for --input-sorted.
-# speeds up step by assuming the input bed files are sorted.
-./find_overlap.py -d
+./find_overlap.py
 # -k flag is short for --skip-series-info.
 # speeds up step by not downloading info on series matrix file sizes.
 # note: use -s flag to include all GEO Series not just DataSets.
